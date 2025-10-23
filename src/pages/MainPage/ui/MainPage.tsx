3dfe73084937f72647aa70d6b54b1042a7f14328
Николай С <nicksrnk@gmail.com>
2025-10-23 21:43:33 +0300

    sys command

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,13 +1,30 @@
 import {Flex} from 'antd';
 import { LeftMenu } from './LeftMenu';
 import { ChatWindow } from './ChatWindow';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+declare global {
+  interface Window {
+    electronAPI: {
+      executeCommand: (command: string) => Promise<string>;
+    };
+  }
+}
 
 const MainPage = () => {
 
     // States
     const [selectedChatId, setSelectedChatId] = useState<number | null>(null);
     // -----
+
+
+    useEffect(() => {
+        if (window.electronAPI) {
+            window.electronAPI.executeCommand('echo "Приложение запущено!"')
+            .then(result => alert('Команда выполнена:' + result))
+            .catch(error => alert('Ошибка:' + error));
+    }
+    }, []);
 
     return(
         <Flex style={{background: '#d8e3f4', height: '100vh', overflow: 'hidden'}}>
